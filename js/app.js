7150c140ecebcdae7eefc48acd3a39eb88eab0f3
Derik <derikhill05@gmail.com>
2015-10-10 01:03:17 -0500

    Added interactivity

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+	gameIntro();
   $('.ryu').mouseenter(function() {
   	$('.ryu-still').hide();
   	$('.ryu-ready').show();
@@ -22,6 +23,17 @@ $(document).ready(function() {
   .mouseup(function() {
   	$('.ryu-throwing').hide();
   	$('.ryu-ready').show();
+  })
+  $(document).keydown(function(e) {
+  	if (e.which == 88) {
+  		$('.ryu-still').hide();
+  		$('.ryu-cool').show();
+  		console.log('button pressed');
+  	};
+  })
+  $(document).keyup(function(e) {
+  	$('.ryu-cool').hide();
+  	$('.ryu-still').show();
   });
 });
 
@@ -29,4 +41,14 @@ function playHadouken() {
 	$('#hadouken-sound')[0].volume = 0.5;
 	$('#hadouken-sound')[0].load();
 	$('#hadouken-sound')[0].play();
+}
+
+function gameIntro() {
+	$('.logo').fadeIn(2500, function() {
+		$(this).fadeOut(1000, function() {
+			$('.instructions').fadeIn(1500, function() {
+				$(this).fadeOut(4500);
+			});
+		});
+	})
 }
